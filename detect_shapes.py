17099ff57cccc49d8fd73b1675f286aad10832d1
tianying <miya00678@gmail.com>
2018-10-25 16:29:41 +0800

    Improved detect_shapes

--- detect_shapes.py
+++ detect_shapes.py
@@ -5,28 +5,44 @@
 # import the necessary packages
 from shapedetector import ShapeDetector
 #import argparse
+import numpy as np
 import imutils
 import cv2
 
-# load the image and resize it to a smaller factor so that
-# the shapes can be approximated better
-image = cv2.imread("tic.png")
-# Apply the background image to mask the image
-bg = cv2.imread("tic_bg.png")
-fgmask = image - bg
 
-resized = imutils.resize(fgmask, width=300)
+# load the image and background
+image = cv2.imread("game.png")
+bg = cv2.imread("board.png")
+
+# Apply same process to the background
+resized_bg = imutils.resize(bg, width=300)
+ratio_bg = bg.shape[0] / float(resized_bg.shape[0])
+
+gray_bg = cv2.cvtColor(resized_bg, cv2.COLOR_BGR2GRAY)
+blurred_bg = cv2.GaussianBlur(gray_bg, (5, 5), 0)
+thresh_bg = cv2.threshold(blurred_bg, 60, 255, cv2.THRESH_BINARY)[1]
+#swap color
+thresh_bg = ~thresh_bg
+
+# resize it to a smaller factor so that
+# the shapes can be approximated better
+#convert the resized image to grayscale, blur it slightly,
+# and threshold it
+resized = imutils.resize(image, width=300)
 ratio = image.shape[0] / float(resized.shape[0])
 
-# convert the resized image to grayscale, blur it slightly,
-# and threshold it
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+#swap color
+thresh = ~thresh
+
+#result image
+thresh_result = thresh - thresh_bg
 
 # find contours in the thresholded image and initialize the
 # shape detector
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+cnts = cv2.findContours(thresh_result.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 sd = ShapeDetector()
@@ -46,8 +62,8 @@ for c in cnts:
 	c *= ratio
 	c = c.astype("int")
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	print(cX)
-	print(cY)
+	#print(cX)
+	#print(cY)
 	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
 		0.5, (255, 255, 255), 2)
 
